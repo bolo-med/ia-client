a605ab8b90ec42ea3865eb7fa08578710d337eba
bolo-med <bosko.medojevic@t-com.me>
2020-07-25 19:54:06 +0200

    dodata komponenta automobili

---
+++ src/app/components/automobili/automobili.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-automobili',
+  templateUrl: './automobili.component.html',
+  styleUrls: ['./automobili.component.scss']
+})
+export class AutomobiliComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
